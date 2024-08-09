21d64cf76eaa3eb176afd18dbd01e5486dcef173
Truth is a pathless land <shubhamtemak99@gmail.com>
2024-08-10 02:38:16 +0530

    without sideeffect loadded the state and loading fully fuctional

--- src/app/components/checkBox.tsx
+++ src/app/components/checkBox.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { atom, useRecoilStateLoadable, useRecoilState } from "recoil";
+import { atom, useRecoilStateLoadable, useRecoilState, selector, useRecoilValueLoadable } from "recoil";
 import { Category } from "@prisma/client";
 import { useEffect } from "react";
 import { getAllCategories } from "../serverUtils/cat";
@@ -9,32 +9,49 @@ export const catIListatom = atom<number>({
   default: undefined,
 });
 
-const listatom = atom<Category[]>({
-  key: "listatom",
-  default: undefined,
-});
+// const listatom = atom<Category[]>({
+//   key: "listatom",
+//   default: undefined,
+// });
 
+const listSelector = selector<Category[]>({
+  key: "listSelector",
+  get: async () => {
+    const res = await getAllCategories(); // Fetch data from server
+    return res // Ensure it's always an array
+  },
+});
 
 export const Checkbox = () => {
   const [catIdList, setCatIdList] = useRecoilStateLoadable(catIListatom);
-  const [list, setList] = useRecoilStateLoadable(listatom);
+  // const [list, setList] = useRecoilStateLoadable(listatom);
+  const list = useRecoilValueLoadable(listSelector)
 
-  useEffect(() => {
-    async function loadeCatIds() {
-      const res = await getAllCategories();
-      setList(res);
-    }
-    loadeCatIds();
-  }, []);
+  // useEffect(() => {
+  //   async function loadeCatIds() {
+  //     const res = await getAllCategories();
+  //     setList(res);
+  //   }
+  //   loadeCatIds();
+  // }, []);
 
   const handleChange = (id: number) => {
     if (catIdList.state === "hasValue") {
         setCatIdList(id);  
     }
   };
-  
+  if (list.state ==="hasValue"){
 
-  if (list.state === "loading" || list.contents === undefined) {
+    return (
+      <>
+        {list.contents.map((item) => (
+          <button className=" bg-black p-5 text-white" onClick={()=>handleChange(item.id)}>{item.name}</button>
+        ))}
+      </>
+    );
+  }
+
+  if (list.state === "loading") {
     return <div className=" bg-slate-600 text-white">Loading...</div>;
   }
 
@@ -42,11 +59,5 @@ export const Checkbox = () => {
     return <div>Error loading categories</div>;
   }
 
-  return (
-    <>
-      {list.contents.map((item) => (
-        <button className=" bg-black p-5 text-white" onClick={()=>handleChange(item.id)}>{item.name}</button>
-      ))}
-    </>
-  );
-};
+  
+  };
