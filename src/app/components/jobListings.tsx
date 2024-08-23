a41bbf527867b01230ac09d413c52d63e843abc5
Truth is a pathless land <shubhamtemak99@gmail.com>
2024-08-23 06:18:26 +0530

    ui changes

--- src/app/components/jobListings.tsx
+++ src/app/components/jobListings.tsx
@@ -12,6 +12,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
+import { SlectedJobDrower } from "./slectedJobDrower"
 
 
   export  default function JobListings() {
@@ -19,7 +20,7 @@ import { Badge } from "@/components/ui/badge"
 
   return (
     <>
-            <Card className=" p-5">
+            <Card className=" p-5 bg-orange-500 min-w-[366px]">
                 <CardHeader>
                     <CardTitle>Create project</CardTitle>
                     <CardDescription>Deploy your new project in one-click.</CardDescription>
@@ -28,13 +29,73 @@ import { Badge } from "@/components/ui/badge"
                  
                 </CardContent>
                 <CardFooter className="flex justify-between">
-                    <Button>Deploy</Button>
+
+                <SlectedJobDrower /> {/* for non mobile devices */}
+
+                <Button className=" hidden md:block" variant={"destructive" }>Open</Button>
+
                     <div className=" flex gap-2">
                         <Badge>Badge</Badge>
                         <Badge>Badge</Badge>
                         <Badge>Badge</Badge>
                     </div>
                 </CardFooter>
+           
+            </Card>
+            
+
+
+
+
+            <Card className=" p-5 bg-orange-500 min-w-[366px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                 
+                </CardContent>
+                <CardFooter className="flex justify-between">
+
+                <SlectedJobDrower /> {/* for non mobile devices */}
+                
+                <Button className=" hidden md:block" variant={"destructive" }>Open</Button>
+
+                    <div className=" flex gap-2">
+                        <Badge>Badge</Badge>
+                        <Badge>Badge</Badge>
+                        <Badge>Badge</Badge>
+                    </div>
+                </CardFooter>
+           
+            </Card>
+
+
+
+
+
+
+            <Card className=" p-5 bg-orange-500 min-w-[366px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                 
+                </CardContent>
+                <CardFooter className="flex justify-between">
+
+                <SlectedJobDrower /> {/* for non mobile devices */}
+                
+                <Button className=" hidden md:block" variant={"destructive" }>Open</Button>
+
+                    <div className=" flex gap-2">
+                        <Badge>Badge</Badge>
+                        <Badge>Badge</Badge>
+                        <Badge>Badge</Badge>
+                    </div>
+                </CardFooter>
+           
             </Card>
     </>
   )
